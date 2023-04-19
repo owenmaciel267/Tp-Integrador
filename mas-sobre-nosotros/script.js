103563168aa335264804eb2ecd50d4d68b1891f6
owenmaciel267 <owentiburon32@gmail.com>
2023-04-19 19:25:13 -0300

    agregue un favicon a la pestaña

--- mas-sobre-nosotros/script.js
+++
@@ -1,9 +0,0 @@
-const hambur = document.getElementById("hambur")
-const navBar = document.getElementById("navBar")
-
-hambur.addEventListener("click", ()=>{
-    navBar.classList.toggle("activee")
-
-    console.log("click");
-})
-
