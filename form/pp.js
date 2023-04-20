e594d5d09c16251a034fa32edf61fb7028c7346f
owenmaciel267 <owentiburon32@gmail.com>
2023-04-20 15:08:33 -0300

    cambie las opciones del formulario de compra

--- form/pp.js
+++ form/pp.js
@@ -1,18 +1,5 @@
 // al hacer scroll que cambie de color
 
-window.addEventListener('scroll', function() {
-    const menu = document.getElementById('menu');
-    const submenu = document.getElementById("submenu")
-    const scrollPosition = window.scrollY;
-
-    
-    if (scrollPosition > 100) {
-      menu.classList.add('scrolled');
-    } else {
-      menu.classList.remove('scrolled');
-    }
-
-})
 
 // menu
 const hambur = document.getElementById("hambur")
@@ -21,4 +8,23 @@ const navBar = document.getElementById("navBar")
 hambur.addEventListener("click", ()=>{
     navBar.classList.toggle("active")
     console.log("click");
+})
+
+let mandandoDatos = document.getElementById("mandandoDatos")
+
+mandandoDatos.addEventListener("click", (event) => {
+  console.log("dobleclick");
+  // event.preventDefault()
+
+  const datos = {
+    nombre: document.getElementById("nombre").value,
+    apellido: document.getElementById("apellido").value,
+    email: document.getElementById("email").value,
+    telefono: document.getElementById("telefono").value,
+    direccion: document.getElementById("direccion").value,
+    formaPago: document.getElementById("formaPago").value,
+    modelo: document.getElementById("modelo").value,
+  }
+
+  console.log(datos);
 })
